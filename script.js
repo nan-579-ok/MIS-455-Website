12970747232db03670af33cddd778b1075933bb2
Nanjeeba <abnanjeeba@gmail.com>
2026-07-05 00:18:11 +0600

    js update

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Welcome to TechNova Store!");
+}
+
+function showOffer() {
+    alert("Today's Offer: Get 10% OFF on all products!");
+}
+
+function contactMessage() {
+    alert("Thank you! Your message has been received.");
+}
